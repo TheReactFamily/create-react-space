0d4f0e323d0205742e022582fbb534e53b24d7a0
Arrastia <arrastia@pm.me>
2022-01-16 20:02:38 +0100

    UPDATE - validate nodeVersion at the entry point

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
 #!/usr/bin/env node
 import { app } from './app';
 
+import { SUPPORTED_NODE_VERSION } from './configuration/constants/compatibility';
+
+import { outdatedNodeVersion } from './validations/outdatedNodeVersion';
+
+const nodeVersion = process.versions.node;
+
+if (parseInt(nodeVersion) < SUPPORTED_NODE_VERSION) {
+  outdatedNodeVersion(nodeVersion);
+}
+
 app();
