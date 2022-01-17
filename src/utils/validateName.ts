549e33286bbd1fbc9346858a25588e6db7693a6a
Arrastia <arrastia@pm.me>
2022-01-17 17:29:18 +0100

    UPDATE - validateName function to pass the test

--- src/utils/validateName.ts
+++ src/utils/validateName.ts
@@ -1,3 +1,7 @@
-export const validateName = (name: string) => {
-  return;
+export const validateName = (name: string): string | boolean => {
+  if (typeof name !== 'string' || name === '') return 'The project name cannot be empty.';
+
+  if (!/^[a-z0-9@.\-_]+$/i.test(name)) return 'The project name can only contain URL-friendly characters.';
+
+  return true;
 };
