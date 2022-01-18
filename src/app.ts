bae4d2a65e89189c23609d9d02d0c4602ead3be9
Arrastia <arrastia@pm.me>
2022-01-18 19:33:55 +0100

    ADD - create new Command program

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,38 @@
+import { Command } from 'commander';
+import { cyan, green } from 'chalk';
+
+import { run } from './services/run';
+
+import type { IProgram } from './@types/Program';
+
 export function app() {
-  console.log('Initializing the app.... 🛩');
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const packageJSON = require('../package.json');
+
+  let inputPath = '';
+
+  const program: IProgram = new Command(packageJSON.name)
+    .version(packageJSON.version)
+    .arguments('<project-root>')
+    .usage(`${cyan('<project-root>')} [options]`)
+    .description('Creates a react space')
+    .option('--help', 'Display help menu')
+    .option('--use-npm', 'Use npm to install dependencies. (Default when Yarn is not installed)')
+    .option('-y, --yes', 'Use the default options to create the space')
+    .allowUnknownOption()
+    .action(projectRoot => (inputPath = projectRoot))
+    .parse(process.argv);
+
+  if (inputPath === '') {
+    console.error('Please specify the project directory:');
+    console.log(`  ${cyan(program.name())} ${green('<project-directory>')}`);
+    console.log();
+    console.log('For example:');
+    console.log(`  ${cyan(program.name())} ${green('my-react-app')}`);
+    console.log();
+    console.log(`Run ${cyan(`${program.name()} --help`)} to see all options.`);
+    process.exit(1);
+  }
+
+  run({ inputPath, program });
 }
