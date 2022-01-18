f901bf48b484f77a284d8a741d072f2abcbc2209
Arrastia <arrastia@pm.me>
2022-01-18 17:44:26 +0100

    UPDATE - getDirectoryConflicts util

--- src/utils/getDirectoryConflicts.ts
+++ src/utils/getDirectoryConflicts.ts
@@ -2,6 +2,6 @@ import { readdirSync } from 'fs-extra';
 
 import { SUPPORTED_FILES } from '../configuration/constants/compatibility';
 
-export const getDirectoryConflicts = (directory: string) => {
+export const getDirectoryConflicts = (directory: string): string[] => {
   return readdirSync(directory).filter((file: string) => !(/\.iml$/.test(file) || SUPPORTED_FILES.includes(file)));
 };
