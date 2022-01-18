9d55eb49d910a98f16ded1a223637027158e6c7f
Arrastia <arrastia@pm.me>
2022-01-18 17:45:45 +0100

    ADD - assertValidName helper

---
+++ src/helpers/assertValidName.ts
@@ -0,0 +1,12 @@
+import { red } from 'chalk';
+
+import { validateName } from '../utils/validateName';
+
+export const assertValidName = (folderName: string) => {
+  const validation = validateName(folderName);
+
+  if (typeof validation === 'string') {
+    console.log(`Cannot create an app named ${red(`"${folderName}"`)}. ${validation}`);
+    process.exit(1);
+  }
+};
