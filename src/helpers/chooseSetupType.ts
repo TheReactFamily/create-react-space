0abd78ae572cc7c8a91343986ef10bc782768f77
Arrastia <arrastia@pm.me>
2022-01-20 18:01:29 +0100

    UPDATE - refactor chooseStartingPoint & rename to chooseSetupType

--- src/helpers/chooseSetupType.ts
+++ src/helpers/chooseSetupType.ts
@@ -4,15 +4,19 @@ import { TEMPLATES_REPOSITORY } from '../configuration/constants/links';
 
 import { terminalLink } from '../components/terminalLink';
 
-export const chooseStartingPoint = async () => {
+import type { SpaceSetupType } from '../@types/Space';
+
+type Choice = { title: string; value: SpaceSetupType };
+
+export const chooseSetupType = async () => {
   const link = terminalLink({ text: 'spaces/templates', url: TEMPLATES_REPOSITORY });
 
-  const choices = [
-    { title: 'Default new app', value: 'default' },
-    { title: `Template from ${link}`, value: 'example' }
+  const choices: Choice[] = [
+    { title: 'Default new app', value: 'DEFAULT' },
+    { title: `Template from ${link}`, value: 'TEMPLATE' }
   ];
 
   const { value } = await prompts({ choices, limit: 11, message: 'How would you like to start', name: 'value', type: 'select' });
 
-  return value;
+  return value as SpaceSetupType;
 };
