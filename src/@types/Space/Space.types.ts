6772c87bdb701bea37d0d03c390bcf6d0f6b9528
Arrastia <arrastia@pm.me>
2022-01-26 20:35:21 +0100

    UPDATE - Space types values

--- src/@types/Space/Space.types.ts
+++ src/@types/Space/Space.types.ts
@@ -1,3 +1,7 @@
-export type SpaceLanguage = 'JAVASCRIPT' | 'TYPESCRIPT';
+export type SpaceLanguage = 'js' | 'ts';
 
-export type SpaceSetupType = 'DEFAULT' | 'TEMPLATE';
+export type SpaceSetupType = 'CRA' | 'DEFAULT' | 'TEMPLATE';
+
+export type Templates = 'cra-template' | 'default' | ResolvedTemplates;
+
+export type ResolvedTemplates = 'basic-error-boundary' | 'basic-recoil' | 'complete-recoil' | 'complete-styled-components';
--- src/helpers/chooseLanguage.ts
+++ src/helpers/chooseLanguage.ts
@@ -6,8 +6,8 @@ type Choice = { title: string; value: SpaceLanguage };
 
 export const chooseLanguage = async () => {
   const choices: Choice[] = [
-    { title: 'Typescript', value: 'TYPESCRIPT' },
-    { title: 'Javascript', value: 'JAVASCRIPT' }
+    { title: 'Typescript', value: 'ts' },
+    { title: 'Javascript', value: 'js' }
   ];
 
   return await promptSelect({ message: 'Which language would you like to use?', options: choices });
