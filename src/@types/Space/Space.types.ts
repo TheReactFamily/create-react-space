c9dfd730b1f4f375642c11333751d2356a88c05f
Arrastia <arrastia@pm.me>
2022-01-27 23:58:10 +0100

    ADD - package related types

--- src/@types/Space/Space.types.ts
+++ src/@types/Space/Space.types.ts
@@ -5,3 +5,15 @@ export type SpaceSetupType = 'CRA' | 'DEFAULT' | 'TEMPLATE';
 export type Templates = 'cra-template' | 'default' | ResolvedTemplates;
 
 export type ResolvedTemplates = 'basic-error-boundary' | 'basic-recoil' | 'complete-recoil' | 'complete-styled-components';
+
+export interface ConfigurationTemplate {
+  package?: ConfigurationPackage;
+}
+
+type PackageEntry = { [name: string]: string };
+
+export type PackageThings = 'dependencies' | 'devDependencies' | 'eslintConfig' | 'scripts';
+
+export type ConfigurationPackage = {
+  [key in PackageThings]?: PackageEntry;
+};
