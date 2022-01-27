77f20ed3426d779e805defed9ad4a94a2309a6f7
Arrastia <arrastia@pm.me>
2022-01-27 23:57:28 +0100

    ADD . PACKAGE_KEYS_TO_MERGE && PACKAGE_KEYS_TO_IGNORE constants

--- src/configuration/constants/dependencies/dependencies.constant.ts
+++ src/configuration/constants/dependencies/dependencies.constant.ts
@@ -1,1 +1,29 @@
 export const FIXED_DEPENDENCIES = ['react', 'react-dom', 'react-scripts'];
+
+export const PACKAGE_KEYS_TO_MERGE = ['dependencies', 'scripts'];
+
+export const PACKAGE_KEYS_TO_IGNORE = [
+  'author',
+  'bin',
+  'browser',
+  'bugs',
+  'bundledDependencies',
+  'contributors',
+  'cpu',
+  'description',
+  'directories',
+  'engineStrict',
+  'files',
+  'keywords',
+  'license',
+  'man',
+  'name',
+  'optionalDependencies',
+  'os',
+  'peerDependencies',
+  'preferGlobal',
+  'private',
+  'publishConfig',
+  'repository',
+  'version'
+];
