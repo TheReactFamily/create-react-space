ebb4aa93b5a82ef3eead726ff78795cf008320af
Arrastia <arrastia@pm.me>
2022-02-05 17:33:06 +0100

    UPDATE - in progress code

--- src/helpers/chooseSetupType.ts
+++ src/helpers/chooseSetupType.ts
@@ -1,20 +1,20 @@
 import { promptSelect } from '../components/promptSelect';
 
-import { TEMPLATES_REPOSITORY } from '../configuration/constants/links';
+// import { TEMPLATES_REPOSITORY } from '../configuration/constants/links';
 
-import { terminalLink } from '../components/terminalLink';
+// import { terminalLink } from '../components/terminalLink';
 
 import type { SpaceSetupType } from '../@types/Space';
 
 type Choice = { title: string; value: SpaceSetupType };
 
 export const chooseSetupType = async () => {
-  const link = terminalLink({ text: 'spaces/templates', url: TEMPLATES_REPOSITORY });
+  // const link = terminalLink({ text: 'spaces/templates', url: TEMPLATES_REPOSITORY });
 
   const choices: Choice[] = [
     { title: 'Default, clean new space', value: 'DEFAULT' },
-    { title: 'Classic create-react-app', value: 'CRA' },
-    { title: `Template from ${link}`, value: 'TEMPLATE' }
+    { title: 'Classic create-react-app', value: 'CRA' }
+    // { title: `Template from ${link}`, value: 'TEMPLATE' }
   ];
 
   return await promptSelect({ limit: 15, message: 'How would you like to start', options: choices });
