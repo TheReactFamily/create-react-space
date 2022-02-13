2b8142ed2f29745f5659b593637a59c97d412c5a
Arrastia <arrastia@pm.me>
2022-02-13 20:23:34 +0100

    REFACTOR - ReactUtils (isReactInstalled)

--- src/utils/ReactUtils/isReactInstalled.ts
+++ src/utils/ReactUtils/isReactInstalled.ts
@@ -1,11 +1,4 @@
-interface appPackage {
-  dependencies?: {
-    ['react-dom']?: string;
-    ['react']?: string;
-  };
-}
-
-export function isReactInstalled(appPackage: appPackage) {
+export function isReactInstalled(appPackage: { dependencies?: { ['react-dom']?: string; ['react']?: string } }) {
   const dependencies = appPackage.dependencies || {};
 
   return typeof dependencies['react'] !== 'undefined' && typeof dependencies['react-dom'] !== 'undefined';
