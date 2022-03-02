9083edf55b9c0518e7011840a9fd5ac0a9a64ce9
Arrastia <arrastia@pm.me>
2022-03-02 17:48:44 +0100

    UPDATE - inner README.md files content

--- src/utils/GitUtils/tryGitCommit.ts
+++ src/utils/GitUtils/tryGitCommit.ts
@@ -5,7 +5,7 @@ import { removeSync } from 'fs-extra';
 export function tryGitCommit(appPath: string) {
   try {
     execSync('git add -A', { stdio: 'ignore' });
-    execSync('git commit -m "Initialize project using Create React App"', { stdio: 'ignore' });
+    execSync('git commit -m "ADD - React project"', { stdio: 'ignore' });
     return true;
   } catch (error) {
     console.warn('Git commit not created', error);
